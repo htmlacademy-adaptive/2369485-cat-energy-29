a233cb9c3dbb895531e0d8be2e7b5a560eea48dc
Astsatur <ogandzanov2@mail.ru>
2023-08-03 16:56:05 +0300

    Испарвляет синтаксис

--- source/js/script.js
+++ source/js/script.js
@@ -2,5 +2,5 @@ let buttonTog = document.querySelector(".main-nav__toggle");
 let navigation = document.querySelector(".site-list");
 
 buttonTog.onclick = function(){
-  navigation.classList.toggle('site-list--hidden');
+  navigation.classList.toggle("site-list--hidden");
 };
