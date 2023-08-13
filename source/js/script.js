4812ed872498f10d5fc18d0a51babef4ba9ca837
Astsatur <ogandzanov2@mail.ru>
2023-08-13 11:26:26 +0300

    Corrects after checking by a mentor

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,6 @@ let burgerSpan =  document.querySelector (".main-nav__burger");
 
 buttonTog.onclick = function(){
   navigation.classList.toggle("site-list--hidden");
-  burgerSpan.classList.toggle("main-nav__burger-rendering");
-  burgerSpan.classList.toggle("main-nav__burger-close");
+  burgerSpan.classList.toggle("main-nav__burger--rendering");
+  burgerSpan.classList.toggle("main-nav__burger--close");
 };
