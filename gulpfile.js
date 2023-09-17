930ccd6cbc67466eb750e92ea142d755dab949f9
Astsatur <ogandzanov2@mail.ru>
2023-09-17 21:50:20 +0300

    changing

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,95 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import csso from 'postcss-csso';
-import rename from 'gulp-rename';
-import browser from 'browser-sync';
-import htmlmin from 'gulp-htmlmin';
-import terser from 'gulp-terser';
-import svgstore from 'gulp-svgstore';
-import svgo from 'gulp-svgmin';
-import squoosh from 'gulp-libsquoosh';
-import del from 'del';
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import browser from "browser-sync";
+import htmlmin from "gulp-htmlmin";
+import terser from "gulp-terser";
+import svgstore from "gulp-svgstore";
+import svgo from "gulp-svgmin";
+import squoosh from "gulp-libsquoosh";
+import del from "del";
 
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp.src("source/sass/style.scss", { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([
       autoprefixer(),
       csso()
     ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
 }
 
 // HTML
 export const html = () => {
-  return gulp.src('source/*.html')
+  return gulp.src("source/*.html")
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest("build"));
 }
 
 // Scripts
 export const scripts = () => {
-  return gulp.src('source/js/*.js')
+  return gulp.src("source/js/*.js")
   .pipe(terser())
-  .pipe(gulp.dest('build/js'))
+  .pipe(gulp.dest("build/js"))
 }
 
 // Images
 export const optimizeImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
+  return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(squoosh())
-  .pipe(gulp.dest('build/img'))
+  .pipe(gulp.dest("build/img"))
 }
 
 export const copyImages = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
-  .pipe(gulp.dest('build/img'))
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(gulp.dest("build/img"))
 }
 
 // Webp
 export const createWebp = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
+  return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(squoosh({webp: {}}))
-  .pipe(gulp.dest('build/img'))
+  .pipe(gulp.dest("build/img"))
 }
 
 // SVG
 export const svg = () =>
-  gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*.svg'])
+  gulp.src(["source/img/**/*.svg", "!source/img/sprite-icons/*.svg"])
     .pipe(svgo())
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest("build/img"));
 
 export const sprite = () =>
-  gulp.src(['source/img/sprite-icons/*.svg'])
+  gulp.src(["source/img/sprite-icons/*.svg"])
     .pipe(svgo())
     .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 
 //Copy
 export const copy = (done) => {
   gulp.src([
-    'source/fonts/**/*.{woff2,woff}',
-    'source/*.ico',
-    'source/manifest.webmanifest',
+    "source/fonts/**/*.{woff2,woff}",
+    "source/*.ico",
+    "source/manifest.webmanifest",
   ], {
-    base: 'source'
+    base: "source"
   })
-  .pipe(gulp.dest('build'))
+  .pipe(gulp.dest("build"))
   done();
 }
 
 //Clean
 export const clean = () => {
-  return del('build');
+  return del("build");
 };
 
 // Server
@@ -97,7 +97,7 @@ export const clean = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -116,8 +116,8 @@ export const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html").on("change", browser.reload);
 }
 
 // Build
